94a51306c698213a8ce95526faff0a29a3187342
Your Name <you@example.com>
2015-09-01 14:44:50 -0400

    commit de tous les dossiers

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 require_once 'data/_data.php';
 $emp_id = 102; // Employé "par défaut"
 // TODO: Lire et mettre à jour l'id de l'employé en Query string
+
+
 $liste_employes = get_employes(); // Liste des noms de tous les employés
 $emp_data = $liste_employes[$emp_id];
 $agenda = get_agenda($emp_id);
 ?>
+<!DOCTYPE html>
 <html>
 	<head>
         <meta charset = "UTF-8" />
@@ -16,9 +19,16 @@ $agenda = get_agenda($emp_id);
     <h2>Activité de l'employé <?php echo $emp_data['emp_name'] ?> </h2>
 	<div id="agenda">
 		<table>
-			<tr><th>Heure</th><th>Activité</th></tr>	
+			<tr><th>Heure</th><th>Activité</th></tr>
+
 <?php
+	//var_dump($_GET);
 // TODO: Afficher l'agenda de l'employé
+foreach ( $agenda as $heure => $activite ) {
+	echo '<tr><th>', $heure ,'</th><th>', $activite,'</th></tr>';
+}
+	echo $emp_data['emp_name'];
+
 ?>
 		</table>
 	</div>
--- view_bloc/_view_header.php
+++ view_bloc/_view_header.php
@@ -2,11 +2,14 @@
     <div id="logo"></div>
     <div id="nom_clinique"><span><?php echo 'Gestion agendas de la ' , NOM_CLINIQUE; ?></span></div>
     <div id="menu">
-    <ul>
-        <?php
-        // TODO: Afficher le menu-liste des employés
-        ?>
-    </ul>
+        <ul>
+            <?php
+            // TODO: Afficher le menu-liste des employés
+            foreach ($liste_employes as $emp_id => $emp_data) {
+                echo '<li><a href="?', PARAM_EMP_ID,'=', $emp_id, '">', $emp_data['emp_name'], '</a></li>';
+            }
+            ?>
+        </ul>
     </div>
 
 </header>
